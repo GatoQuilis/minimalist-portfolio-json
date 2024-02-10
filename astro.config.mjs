0b1ca4c9397465f29fe61bdd0372cabe5dd3155c
Franco Diaz Quilis <gatoquilis@MacBook-Pro-de-GATO.local>
2024-02-10 16:40:42 -0300

    Configurando Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://GatoQuilis.github.com'
+    site: 'https://GatoQuilis.github.com',
+    base: '/minimalist-portfolio-json',
 });
